6ede09cbd6dd4ffba2d2bad263a25b1840a7f481
Donnie Adams <donnie@acorn.io>
2025-01-14 08:09:04 -0500

    feat: add file revision API for workspaces
    Signed-off-by: Donnie Adams <donnie@acorn.io>

--- workspace_test.go
+++ workspace_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"context"
 	"errors"
+	"fmt"
 	"os"
 	"testing"
 )
@@ -88,6 +89,98 @@ func TestWriteReadAndDeleteFileFromWorkspace(t *testing.T) {
 	err = g.DeleteFileInWorkspace(context.Background(), "test.txt", DeleteFileInWorkspaceOptions{WorkspaceID: id})
 	if err != nil {
 		t.Errorf("Error deleting file: %v", err)
+	}
+}
+
+func TestRevisionsForFileInWorkspace(t *testing.T) {
+	id, err := g.CreateWorkspace(context.Background(), "directory")
+	if err != nil {
+		t.Fatalf("Error creating workspace: %v", err)
+	}
+
+	t.Cleanup(func() {
+		err := g.DeleteWorkspace(context.Background(), id)
+		if err != nil {
+			t.Errorf("Error deleting workspace: %v", err)
+		}
+	})
+
+	err = g.WriteFileInWorkspace(context.Background(), "test.txt", []byte("test0"), WriteFileInWorkspaceOptions{WorkspaceID: id})
+	if err != nil {
+		t.Fatalf("Error creating file: %v", err)
+	}
+
+	err = g.WriteFileInWorkspace(context.Background(), "test.txt", []byte("test1"), WriteFileInWorkspaceOptions{WorkspaceID: id})
+	if err != nil {
+		t.Fatalf("Error creating file: %v", err)
+	}
+
+	err = g.WriteFileInWorkspace(context.Background(), "test.txt", []byte("test2"), WriteFileInWorkspaceOptions{WorkspaceID: id})
+	if err != nil {
+		t.Fatalf("Error creating file: %v", err)
+	}
+
+	revisions, err := g.ListRevisionsForFileInWorkspace(context.Background(), "test.txt", ListRevisionsForFileInWorkspaceOptions{WorkspaceID: id})
+	if err != nil {
+		t.Errorf("Error reading file: %v", err)
+	}
+
+	if len(revisions) != 2 {
+		t.Errorf("Unexpected number of revisions: %d", len(revisions))
+	}
+
+	for i, rev := range revisions {
+		if rev.WorkspaceID != id {
+			t.Errorf("Unexpected file workspace ID: %v", rev.WorkspaceID)
+		}
+
+		if rev.Name != "test.txt" {
+			t.Errorf("Unexpected file name: %s", rev.Name)
+		}
+
+		if rev.Size != 5 {
+			t.Errorf("Unexpected file size: %d", rev.Size)
+		}
+
+		if rev.ModTime.IsZero() {
+			t.Errorf("Unexpected file mod time: %v", rev.ModTime)
+		}
+
+		if rev.MimeType != "text/plain" {
+			t.Errorf("Unexpected file mime type: %s", rev.MimeType)
+		}
+
+		if rev.RevisionID != fmt.Sprintf("%d", i+1) {
+			t.Errorf("Unexpected revision ID: %s", rev.RevisionID)
+		}
+	}
+
+	err = g.DeleteRevisionForFileInWorkspace(context.Background(), "test.txt", "1", DeleteRevisionForFileInWorkspaceOptions{WorkspaceID: id})
+	if err != nil {
+		t.Errorf("Error deleting revision for file: %v", err)
+	}
+
+	revisions, err = g.ListRevisionsForFileInWorkspace(context.Background(), "test.txt", ListRevisionsForFileInWorkspaceOptions{WorkspaceID: id})
+	if err != nil {
+		t.Errorf("Error reading file: %v", err)
+	}
+
+	if len(revisions) != 1 {
+		t.Errorf("Unexpected number of revisions: %d", len(revisions))
+	}
+
+	err = g.DeleteFileInWorkspace(context.Background(), "test.txt", DeleteFileInWorkspaceOptions{WorkspaceID: id})
+	if err != nil {
+		t.Errorf("Error deleting file: %v", err)
+	}
+
+	revisions, err = g.ListRevisionsForFileInWorkspace(context.Background(), "test.txt", ListRevisionsForFileInWorkspaceOptions{WorkspaceID: id})
+	if err != nil {
+		t.Errorf("Error reading file: %v", err)
+	}
+
+	if len(revisions) != 0 {
+		t.Errorf("Unexpected number of revisions: %d", len(revisions))
 	}
 }
 
@@ -243,6 +336,102 @@ func TestWriteReadAndDeleteFileFromWorkspaceS3(t *testing.T) {
 	err = g.DeleteFileInWorkspace(context.Background(), "test.txt", DeleteFileInWorkspaceOptions{WorkspaceID: id})
 	if err != nil {
 		t.Errorf("Error deleting file: %v", err)
+	}
+}
+
+func TestRevisionsForFileInWorkspaceS3(t *testing.T) {
+	if os.Getenv("AWS_ACCESS_KEY_ID") == "" || os.Getenv("AWS_SECRET_ACCESS_KEY") == "" || os.Getenv("WORKSPACE_PROVIDER_S3_BUCKET") == "" {
+		t.Skip("Skipping test because AWS credentials are not set")
+	}
+
+	id, err := g.CreateWorkspace(context.Background(), "s3")
+	if err != nil {
+		t.Fatalf("Error creating workspace: %v", err)
+	}
+
+	t.Cleanup(func() {
+		err := g.DeleteWorkspace(context.Background(), id)
+		if err != nil {
+			t.Errorf("Error deleting workspace: %v", err)
+		}
+	})
+
+	err = g.WriteFileInWorkspace(context.Background(), "test.txt", []byte("test0"), WriteFileInWorkspaceOptions{WorkspaceID: id})
+	if err != nil {
+		t.Fatalf("Error creating file: %v", err)
+	}
+
+	err = g.WriteFileInWorkspace(context.Background(), "test.txt", []byte("test1"), WriteFileInWorkspaceOptions{WorkspaceID: id})
+	if err != nil {
+		t.Fatalf("Error creating file: %v", err)
+	}
+
+	err = g.WriteFileInWorkspace(context.Background(), "test.txt", []byte("test2"), WriteFileInWorkspaceOptions{WorkspaceID: id})
+	if err != nil {
+		t.Fatalf("Error creating file: %v", err)
+	}
+
+	revisions, err := g.ListRevisionsForFileInWorkspace(context.Background(), "test.txt", ListRevisionsForFileInWorkspaceOptions{WorkspaceID: id})
+	if err != nil {
+		t.Errorf("Error reading file: %v", err)
+	}
+
+	if len(revisions) != 2 {
+		t.Errorf("Unexpected number of revisions: %d", len(revisions))
+	}
+
+	for i, rev := range revisions {
+		if rev.WorkspaceID != id {
+			t.Errorf("Unexpected file workspace ID: %v", rev.WorkspaceID)
+		}
+
+		if rev.Name != "test.txt" {
+			t.Errorf("Unexpected file name: %s", rev.Name)
+		}
+
+		if rev.Size != 5 {
+			t.Errorf("Unexpected file size: %d", rev.Size)
+		}
+
+		if rev.ModTime.IsZero() {
+			t.Errorf("Unexpected file mod time: %v", rev.ModTime)
+		}
+
+		if rev.MimeType != "text/plain" {
+			t.Errorf("Unexpected file mime type: %s", rev.MimeType)
+		}
+
+		if rev.RevisionID != fmt.Sprintf("%d", i+1) {
+			t.Errorf("Unexpected revision ID: %s", rev.RevisionID)
+		}
+	}
+
+	err = g.DeleteRevisionForFileInWorkspace(context.Background(), "test.txt", "1", DeleteRevisionForFileInWorkspaceOptions{WorkspaceID: id})
+	if err != nil {
+		t.Errorf("Error deleting revision for file: %v", err)
+	}
+
+	revisions, err = g.ListRevisionsForFileInWorkspace(context.Background(), "test.txt", ListRevisionsForFileInWorkspaceOptions{WorkspaceID: id})
+	if err != nil {
+		t.Errorf("Error reading file: %v", err)
+	}
+
+	if len(revisions) != 1 {
+		t.Errorf("Unexpected number of revisions: %d", len(revisions))
+	}
+
+	err = g.DeleteFileInWorkspace(context.Background(), "test.txt", DeleteFileInWorkspaceOptions{WorkspaceID: id})
+	if err != nil {
+		t.Errorf("Error deleting file: %v", err)
+	}
+
+	revisions, err = g.ListRevisionsForFileInWorkspace(context.Background(), "test.txt", ListRevisionsForFileInWorkspaceOptions{WorkspaceID: id})
+	if err != nil {
+		t.Errorf("Error reading file: %v", err)
+	}
+
+	if len(revisions) != 0 {
+		t.Errorf("Unexpected number of revisions: %d", len(revisions))
 	}
 }
 
